3bb56e06b0e9b06adc30e37da85ae9679b7a9ecf
noname <dynam1001@naver.com>
2020-01-04 22:03:57 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,11 @@
-use std::boxed::Box;
-
 fn main()
 {
-    let v = 1;
-    let v2 = v;
-    println!("v is: {}", v);
-
-    let mut obj = Box::new(0);
-    *obj += 15;
-
-    println!("{}", *obj);
-
-    do_something(&12);
-}
-
-fn do_something(v: & i32) {
-    let c = v + 1;
-    // something with v
+    for i in 0..9
+    {
+        for _j in 0..i
+        {
+            print!("*");
+        }
+        print!("\n");
+    }
 }
