579ff22be6d23780dcc321d1aff7060c0e04e3e2
noname <48761044+noname0310@users.noreply.github.com>
2020-02-01 16:48:03 +0900

    백준 문제풀이

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,114 @@
+use std::io;
+
 fn main()
 {
-    for i in 0..9
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let n: i32 = input.trim().parse().unwrap();
+
+    for i in 1..= n * 2 -1
     {
-        for _j in 0..i
+        if i <= n
         {
-            print!("*");
+            for _j in 0..=i-2
+            {
+                print!(" ");
+            }
+            for j in 1..=n
+            {
+                if i == 1 || i == n * 2 -1
+                {
+                    print!("*");
+                }
+                else
+                {
+                    if j == 1 || j == n
+                    {
+                        print!("*");
+                    }
+                    else
+                    {
+                        print!(" ");
+                    }
+                }
+            }
+            for _j in 0..(n - i) * 2 - 1
+            {
+                print!(" ");
+            }
+            for j in 1..=n
+            {
+                if i == 1 || i == n * 2 -1
+                {
+                    print!("*");
+                }
+                else
+                {
+                    if j == 1
+                    {
+                        if i != n
+                        {
+                            print!("*");
+                        }
+                    }
+                    else if j == n
+                    {
+                        print!("*");
+                    }
+                    else
+                    {
+                        print!(" ");
+                    }
+                }
+            }
         }
-        print!("\n");
+        else
+        {
+            for _j in 0..i - (i - n) * 2 - 1
+            {
+                print!(" ");
+            }
+            for j in 1..=n
+            {
+                if i == 1 || i == n * 2 -1
+                {
+                    print!("*");
+                }
+                else
+                {
+                    if j == 1 || j == n
+                    {
+                        print!("*");
+                    }
+                    else
+                    {
+                        print!(" ");
+                    }
+                }
+            }
+            for _j in 0..(i - n) * 2 - 1
+            {
+                print!(" ");
+            }
+            for j in 1..=n
+            {
+                if i == 1 || i == n * 2 -1
+                {
+                    print!("*");
+                }
+                else
+                {
+                    if j == 1 || j == n
+                    {
+                        print!("*");
+                    }
+                    else
+                    {
+                        print!(" ");
+                    }
+                }
+            }
+        }
+        println!();
     }
 }
