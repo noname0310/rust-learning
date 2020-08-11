a42e327f13de187ace0e9148ed9f21f1ec160e02
noname <48761044+noname0310@users.noreply.github.com>
2020-08-11 15:59:54 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::vec::Vec;
 
 #[inline(always)]
 fn user_inputs<F: std::str::FromStr>() -> Vec<F> {
@@ -14,65 +15,20 @@ fn user_inputs<F: std::str::FromStr>() -> Vec<F> {
 
 fn main() {
 	let input = {
-		let inputs = user_inputs::<usize>();
+		let inputs = user_inputs::<i32>();
 		inputs[0]
 	};
 
-	let mut array = vec![vec![' '; input]; input];
-	array[0][0] = '*'; array[0][1] = '*'; array[0][2] = '*';
-	array[1][0] = '*';                    array[1][2] = '*';
-	array[2][0] = '*'; array[2][1] = '*'; array[2][2] = '*';
-
-	pattern(&mut array, 3, input);
-
-	for elem in array {
-		for elem1 in elem {
-			print!("{}", elem1);
-		}
-		println!();
-	}
+	let mut s: Vec<String> = Vec::new();
+	hanoi(input, 1, 3, 2, &mut s);
 }
 
-fn pattern(array: &mut Vec<Vec<char>>, width: usize, endcond: usize) {
-	if width >= endcond {
+fn hanoi (n: i32, from: i32, target: i32, other: i32, vec: &mut Vec<String>) {
+	if n == 1 {
+		vec.push(String.) println!("{} {}", from, target);
 		return;
 	}
-	for i in 0..width {
-		for j in width..width * 2 {
-			array[i][j] = array[i][j - width];
-		}
-	}
-	for i in 0..width {
-		for j in width * 2..width * 3{
-			array[i][j] = array[i][j - (width * 2)];
-		}
-	}
-
-	for i in width..width * 2 {
-		for j in 0..width {
-			array[i][j] = array[i - width][j];
-		}
-	}
-	for i in width..width * 2 {
-		for j in width * 2..width * 3 {
-			array[i][j] = array[i - width][j - (width * 2)];
-		}
-	}
-	
-	for i in width * 2..width * 3 {
-		for j in 0..width {
-			array[i][j] = array[i - (width * 2)][j];
-		}
-	}
-	for i in width * 2..width * 3 {
-		for j in width..width * 2 {
-			array[i][j] = array[i - (width * 2)][j - width];
-		}
-	}
-	for i in width * 2..width * 3 {
-		for j in width * 2..width * 3 {
-			array[i][j] = array[i - (width * 2)][j - (width * 2)];
-		}
-	}
-	pattern(array, width * 3, endcond);
+	hanoi(n - 1, from, other, target, vec);
+	println!("{} {}", from, target);
+	hanoi(n - 1, other, target, from, vec);
 }
