5f3d01fb6d957e74c36689b2b81e498dd8648685
noname <48761044+noname0310@users.noreply.github.com>
2020-09-09 21:55:09 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::io;
 use std::vec::Vec;
-use std::collections::BinaryHeap;
 
 #[inline(always)]
 fn user_inputs<F: std::str::FromStr>(val: usize) -> Vec<F> {
@@ -15,72 +14,37 @@ fn user_inputs<F: std::str::FromStr>(val: usize) -> Vec<F> {
 }
 
 fn main() {
-	let input = {
-		let inputs = user_inputs::<i32>(2);
-		(inputs[0], inputs[1])
-	};
-	let v = input.0 + 1;
-	let _e = input.1;
-
-	let input = {
-		let inputs = user_inputs::<i32>(1);
+	let n = {
+		let inputs = user_inputs::<usize>(2);
 		inputs[0]
 	};
-	let start = input;
+	let mut val = [0; 27];
+	let mut stack = Vec::new();
 	
-	let max = 20001;
-	let mut adj: Vec<Vec<(i32, i32)>> = vec![vec![(0, 0); 3]; max];
+	let mut str = String::new();
+	let mut a = io::stdin().read_line(&mut str);
 
-	for _i in 0.._e {
-		let input = {
-			let inputs = user_inputs::<i32>(3);
-			(inputs[0], inputs[1], inputs[2])
-		};
-
-		let x = input.0;
-		let y = input.1;
-		let z = input.2;
-
-		adj[x as usize].push((y, z));
+	for i in 0..n {
+		val[i] = user_inputs::<usize>(1)[0];
 	}
 
-	let vec = dijkstra(start as usize, v as usize, adj);
-	for i in 1..v {
-		if vec[i as usize] == std::i32::MAX {
-			println!("INF")
+	for i in 0..str.len() {
+		if str.as_bytes()[i] >= 'A' as u8 && str.as_bytes()[i] <= 'Z' as u8 {
+			let index = str.as_bytes()[i] - 'A' as u8;
+			stack.push(val[index as usize]);
 		} else {
-			println!("{}", vec[i as usize]);
-		}
-	}
-}
+			let temp2 = stack.pop().unwrap();
+			let temp1 = stack.pop().unwrap();
 
-fn dijkstra(src: usize, v: usize, adj: Vec<Vec<(i32, i32)>>) -> Vec<i32> {
-	let mut dist = vec![std::i32::MAX; v];
-	dist[src] = 0;
-
-	let mut pq = BinaryHeap::new();
-	pq.push((0, src));
-
-	while !pq.is_empty() {
-		let cost = -(pq.peek().unwrap().0);
-		let here = pq.peek().unwrap().1;
-
-		pq.pop().unwrap();
-
-		if dist[here] < cost {
-			continue;
-		}
-
-		for i in 0..(adj[here].len()) {
-			let there = adj[here][i].0 as usize;
-			let next = cost + adj[here][i].1;
-
-			if dist[there] > next {
-				dist[there] = next;
-				pq.push((-next, there));
+			if str.as_bytes()[i] == '*' as u8 {
+				stack.push(temp1 * temp2);
+			} else if str.as_bytes()[i] == '+' as u8 {
+				stack.push(temp1 * temp2);
+			} else if str.as_bytes()[i] == '-' as u8 {
+				stack.push(temp1 * temp2);
+			} else if str.as_bytes()[i] == '/' as u8 {
+				stack.push(temp1 * temp2);
 			}
 		}
 	}
-
-	return dist;
 }
